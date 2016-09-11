7ff07811d987279ce447181facc141a17ddf9d64
Maria Mckinley <parody@uw.edu>
2016-09-11 13:45:58 -0700

    trying to update

--- tweet_stuff.py
+++ tweet_stuff.py
@@ -6,16 +6,16 @@ from fhg_tweeter import get_best_headlines
 url = ('http://www.baynews9.com/content'
             '/news/baynews9/feeds/rss.html/strange.html')
 
-headlines = get_best_headlines(url, True)
-if headlines:
-    print(headlines)
 secrets = json.load(open('twitter_keys.json'))
 auth = tweepy.OAuthHandler(secrets['API Key'], secrets['API Secret'])
 auth.secure = True
 auth.set_access_token(secrets['Access Token'], secrets['Access Token Secret'])
 api = tweepy.API(auth)
 
-#api.update_status(new_update)
+headlines = get_best_headlines(url, True)
+if headlines:
+    #print(headlines[0])
+    api.update_status(headlines[0])
 
 #public_tweets = api.home_timeline()
 #for tweet in public_tweets:
